38ba0a87506aecc8ff27d55f092f6462c0c53031
Divanshu Singh <divanshusingh726@gmail.com>
2026-07-23 09:03:49 +0530

    Restore arrays folder

---
+++ arrays/problem4.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+//check if an array is sorted in ascending order
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+    bool found = true;
+    for (int i = 0; i < n; i++){
+        if (arr[i]>arr[i+1]){
+            found = false;
+        }
+    }
+    if (found){
+        cout<<"The array is sorted in ascending order";
+    }
+    else {
+        cout<<"The array is not sorted in ascending order";
+    }
+    return 0;
+}
