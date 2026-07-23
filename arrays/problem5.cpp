6c627a16c762ca15c87411b04a66549241bca96f
Divanshu Singh <divanshusingh726@gmail.com>
2026-07-23 08:27:00 +0530

    add solution of problem 1

---
+++ arrays/problem5.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    cout<<"Enter duplicate no array";
+    for (int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+    int i = 0;
+    for (int j = 1; j < n; j++){
+        if (arr[i]!=arr[j]){
+            i++;
+            arr[i]=arr[j];
+        }
+    }
+    cout<<"Array after removing duplicates: ";
+    for (int k=0; k<=i; k++){
+        cout<<arr[k]<<" ";
+    }
+    return 0;
+}
