0ca853a9bd945b1c6d18d25cda25422ff23e3746
Divanshu Singh <divanshusingh726@gmail.com>
2026-07-23 08:31:46 +0530

    add solution of problem 1

---
+++ strings/problem1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    string str;
+    getline(cin, str);
+    for (int i = 0; i < str.length(); i++){
+        if (str[i] == ' ') {
+            continue;
+        }
+        cout<<str[i]<<"\n";
+    }
+    return 0;
+}
